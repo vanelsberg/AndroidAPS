288bcd4371aa14da2999fde49578e4b6626c60bd
Theo van Elsberg <van.elsberg@outlook.com>
2024-10-09 00:47:06 +0200

    Initial implementation of class SecureEncrypt. Compiles but untested. Needs Work!

--- implementation/src/main/kotlin/app/aaps/implementation/protection/SecureEncryptImpl.kt
+++ implementation/src/main/kotlin/app/aaps/implementation/protection/SecureEncryptImpl.kt
@@ -5,12 +5,11 @@ import app.aaps.core.interfaces.logging.AAPSLogger
 import app.aaps.core.interfaces.logging.LTag
 import app.aaps.core.interfaces.protection.SecureEncrypt
 import app.aaps.core.interfaces.sharedPreferences.SP
-import app.aaps.core.keys.BooleanKey
 import dagger.Reusable
 import javax.inject.Inject
 
 /***
- * EXAMPLE for encrypting using Android KeyStore?
+ * Encrypting secrets using Android KeyStore?
  */
 
 import android.security.keystore.KeyGenParameterSpec
@@ -23,6 +22,9 @@ import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
 
 // Internal constant stings?
+private const val keyAlias = "doTestKeyAlias01"
+private const val prefEncryptionSecret = "export_secret"
+private const val hexStringSeparator = ":"
 
 @Reusable
 class SecureEncryptImpl @Inject constructor(
@@ -32,24 +34,46 @@ class SecureEncryptImpl @Inject constructor(
 
     // TODO: Implementation
     override fun dummy(context: Context) : Boolean {
-        val exportPasswordStoreIsEnabled  = sp.getBoolean(BooleanKey.MaintenanceEnableExportSettingsAutomation.key, false)
         log.debug(LTag.CORE, "TODO: Executing Dummy function...")
         return false
     }
 
     /***
-     * TODO: Preparing for encryption/decryption (needs additional implementation)
+     * TODO: encryption/decryption (needs additional implementation and error/exception handling)
      */
     override fun encrypt(str: String): String {
-        log.debug(LTag.CORE, "encrypting export password.")
-        return str
+        var secret = ""
+        if (!str.isNullOrEmpty()) {
+            // Encrypt original data string
+            val classEncryptedData = keyStoreEncrypt(keyAlias, str)
+            secret = getDataStringFromClassEncryptedData(classEncryptedData)
+            log.debug(LTag.CORE, "Stored encryption secret!?.")
+        }
+        else {
+            log.debug(LTag.CORE, "not encrypting empty secret!?.")
+        }
+        sp.putString(prefEncryptionSecret, secret)
+
+        return secret
     }
 
     override fun decrypt(str: String): String {
-        //test()
-        doTest()
-        log.debug(LTag.CORE, "decrypting export password.")
-        return str
+        // doTest() // TODO: testing only
+
+        // Retrieve encryption secret from preferences
+        var decryptedString = ""
+        val prefSecrets: String = sp.getString(prefEncryptionSecret, "")
+        if (!prefSecrets.isNullOrEmpty()) {
+            // Prepare for decryption
+            val classEncryptedData = putDataStringToClassEncryptedData(prefSecrets)
+            // Decrypt string
+            decryptedString = keyStoreDecrypt(keyAlias, classEncryptedData)
+            log.debug(LTag.CORE, "secret decrypted.")
+        }
+        else
+            log.debug(LTag.CORE, "empty secret not decrypted.")
+
+        return decryptedString
     }
 
 
@@ -57,57 +81,14 @@ class SecureEncryptImpl @Inject constructor(
      * Private functions
     *************************************************************************/
 
-    /***
-     * EXAMPLE for encrypting using Android KeyStore?
-     */
+    private fun getKeyFromKeyStore(keyAlias: String, forceNew: Boolean = false): SecretKey {
+        // TODO: Handle exceptions!?
 
-    // Function to encrypt a string
-    private fun encryptText(keyAlias: String, textToEncrypt: String): String {
-        var encryptedData: String = ""
-        try {
-        }
-        catch (e: Exception) {
-            encryptedData = ""
-            log.error(LTag.CORE, "encryptText: ${e.message}")
-
-        }
-        return encryptedData
-    }
-
-    // Function to decrypt a string
-    private fun decryptText(alias: String, encryptedText: ByteArray): String {
-        var decryptedText: String = ""
-        try {
-        }
-        catch (e: Exception) {
-            log.error(LTag.CORE, "decryptText: ", e)
-        }
-        return decryptedText
-    }
-
-    private fun hexStringToByteArray(hexString: String): ByteArray {
-        val len = hexString.length
-        val data = ByteArray(len / 2)
-        for (i in 0 until len step 2) {
-            data[i / 2] = ((Character.digit(hexString[i], 16) shl 4) + Character.digit(hexString[i + 1], 16)).toByte()
-        }
-        return data
-    }
-
-    @OptIn(ExperimentalStdlibApi::class)
-    private fun getIvHex(iv: ByteArray): String
-    {
-        var ivHex: String = iv.toHexString()
-        //val ivHex = "8daf837291125a3a2a7d435db2369dd8"
-        return ivHex
-    }
-
-    private fun storeSecretKey(keyAlias: String, createNew: Boolean = true): SecretKey {
         val keyStore = KeyStore.getInstance("AndroidKeyStore")
         keyStore.load(null)
         val keyStoreIsAvailable = keyStore.containsAlias(keyAlias)
 
-        if (!keyStoreIsAvailable || createNew) {
+        if (!keyStoreIsAvailable || forceNew) {
             val keyGenParameterSpec = KeyGenParameterSpec.Builder(
                 keyAlias,KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
                 )
@@ -119,11 +100,13 @@ class SecureEncryptImpl @Inject constructor(
             return keyGenerator.generateKey()
         }
         else {
-            return retrieveSecretKey(keyAlias)
+            return retrieveSecretKeyFromKeyStore(keyAlias)
         }
     }
 
-    private fun retrieveSecretKey(keyAlias: String): SecretKey {
+    private fun retrieveSecretKeyFromKeyStore(keyAlias: String): SecretKey {
+        // TODO: Handle exceptions!?
+
         val keyStore = KeyStore.getInstance("AndroidKeyStore")
         keyStore.load(null)
         val secretKeyEntry = keyStore.getEntry(keyAlias, null) as KeyStore.SecretKeyEntry
@@ -131,58 +114,131 @@ class SecureEncryptImpl @Inject constructor(
         return secretKey
     }
 
+    /***
+     * Convert hex string to ByteArray
+     * Returns ByteArray
+     */
+    private fun hexStringToByteArray(hexString: String): ByteArray {
+        val len = hexString.length
+        val data = ByteArray(len / 2)
+        for (i in 0 until len step 2) {
+            data[i / 2] = ((Character.digit(hexString[i], 16) shl 4) + Character.digit(hexString[i + 1], 16)).toByte()
+        }
+        return data
+    }
 
-    // Test/Usage example
+    /***
+     * Data class holding encryption attributes
+     */
+    data class ClassEncryptedData (
+        var ivHexString: String,
+        var encryptedDataHexString: String
+    )
+
+    /**
+     * Get classEncryptedData into one single Sting formatted "<hexdata1><separator><hexdata2>"
+     */
+    private fun getDataStringFromClassEncryptedData(classEncryptedData: ClassEncryptedData): String
+    {
+        return buildString {
+        append(classEncryptedData.ivHexString)
+        append(hexStringSeparator)
+        append(classEncryptedData.encryptedDataHexString)
+        }
+    }
+
+    /**
+     * Input single Sting formatted "<hexdata1><separator><hexdata2>"
+     * Returns initialized ClassEncryptedData object
+     */
+    private fun putDataStringToClassEncryptedData(dataString: String): ClassEncryptedData
+    {
+        val data = dataString.split(hexStringSeparator)
+        return ClassEncryptedData (
+            ivHexString = data[0],
+            encryptedDataHexString = data[1]
+        )
+    }
+
     @OptIn(ExperimentalStdlibApi::class)
-    private fun doTest(): Boolean {
-        val keyAlias = "myTestKeyAlias1"
+    private fun keyStoreEncrypt(keyAlias: String, originalDataString: String): ClassEncryptedData {
+        // TODO: Handle exceptions!
 
         // Get secret key
-        var secretKey = storeSecretKey(keyAlias, createNew = false)
+        val secretKey = getKeyFromKeyStore(keyAlias)
 
         // Initialize Cipher for encryption & get iv
         val cipher = Cipher.getInstance("AES/GCM/NoPadding")
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-        var ivHex = cipher.iv.toHexString()
+        val ivHex = cipher.iv.toHexString()
 
         // Encrypt Data
-        val originalDataString = "My sensitive data"
         val originalData = originalDataString.toByteArray()
         val encryptedData = cipher.doFinal(originalData)
-        var encryptedDataHexString = encryptedData.toHexString()
 
-        /*** Result:
-         * - ivHex
-         * - encryptedDataHexString
-         */
-        println("Encryption, iv(hex)  : $ivHex")
-        println("Encryption, Text(hex): $encryptedDataHexString")
-        /* Logcat:
-        * Encryption, iv(hex)  : eb0b3795249f0286eb1b1d79
-        * Encryption, Text(hex): e2eb6dc55cd09f0a27e2a76944ee14d7b1d6dcb4d6111620237b72633ea62e63b8
-        */
+        val classEncryptedData = ClassEncryptedData (
+            ivHexString = ivHex,
+            encryptedDataHexString = encryptedData.toHexString()
+        )
+        println("Encryption, iv(hex)  : $classEncryptedData.ivHex")
+        println("Encryption, Text(hex): $classEncryptedData.encryptedDataHexString")
+        return classEncryptedData
+    }
 
-        // Now try to decrypt:
+    private fun keyStoreDecrypt(keyAlias: String, classEncryptedData: ClassEncryptedData): String {
+        // TODO: Handle exceptions!
+
         // Get decryption parameters
-        val cipher2 = Cipher.getInstance("AES/GCM/NoPadding")
-        val iv2: ByteArray = ivHex.hexStringToByteArray()
-        val secretKey2: SecretKey = retrieveSecretKey(keyAlias)
-
-        //iv2 = ""
-        //encryptedDataHexString = "bab38811c25356e46277d73c8d304034383e76ac52aead190f75ee8cdcb6c8e2"
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        val iv: ByteArray = classEncryptedData.ivHexString.hexStringToByteArray()
+        val secretKey: SecretKey = retrieveSecretKeyFromKeyStore(keyAlias)
 
         // Initialize Cipher instance we already have?
-        val ivParameterSpec = GCMParameterSpec(128, iv2) // Use GCMParameterSpec
-        cipher2.init(Cipher.DECRYPT_MODE, secretKey2, ivParameterSpec)
+        val ivParameterSpec = GCMParameterSpec(128, iv) // Use GCMParameterSpec
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec)
 
         // Decrypt
-
-        val dataToDecrypt = hexStringToByteArray(encryptedDataHexString)
-        val decryptedData = cipher2.doFinal(dataToDecrypt)
+        val dataToDecrypt = hexStringToByteArray(classEncryptedData.encryptedDataHexString)
+        val decryptedData = cipher.doFinal(dataToDecrypt)
         val decryptedDataString=String(decryptedData)
 
         // Check the result:
         println("Encryption, decrypted text: $decryptedDataString")
+        return decryptedDataString
+    }
+
+    /***
+    * TESTING...
+    */
+
+    // Test/Usage example
+    private fun doTest(): Boolean {
+        val originalDataString = "My sensitive data"
+
+        // Encrypt original data string
+        var classEncryptedData = keyStoreEncrypt(keyAlias, originalDataString)
+        /**
+        Data class classEncryptedData object now holds keystore parameter iv (bytes) and encrypted data (bytes) as Hex String
+        Function keyStoreDecrypt uses the key alias to retrieve the secret key and decipher the encrypted data using
+        the secret key and iv
+        **/
+        // Make it one string for easy storing
+        var testdata:String = getDataStringFromClassEncryptedData(classEncryptedData)
+
+        // Try to get some encrypted data from elsewhere
+        val decryptDebug = false
+        if (decryptDebug) {
+            // Debug: using fixed iv and encrypted string from previous encryption run (see logcat?)
+            val testDataClass = ClassEncryptedData (
+                ivHexString = "833fb80226e34b7289b3cad5",
+                encryptedDataHexString = "f81583199a30642ec9c32ca2fd1d6e23ed15c874c42f2acde9b2b6774b1175fad6"
+            )
+            testdata = getDataStringFromClassEncryptedData(testDataClass)
+        }
+
+        // Now decrypt encryption result in decrypted data to original data string
+        classEncryptedData = putDataStringToClassEncryptedData(testdata)
+        val decryptedDataString = keyStoreDecrypt(keyAlias, classEncryptedData)
 
         if (originalDataString == decryptedDataString) {
             println("Encryption, OK!")
